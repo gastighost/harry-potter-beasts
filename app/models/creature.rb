3678fc8936e0507aa0a1f3059baf3880a5d37066
Gaston Roxas <gacaro51597@gmail.com>
2022-02-22 20:34:12 +0100

    added name to creature model validation

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -3,5 +3,5 @@ class Creature < ApplicationRecord
   has_many :reviews
   has_many :bookings
 
-  validates :category, :breed, :super_power, :needed_years_magic, presence: true
+  validates :name, :category, :breed, :super_power, :needed_years_magic, presence: true
 end
