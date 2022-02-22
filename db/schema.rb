a8cc1b9dc272ed0716386a731b91ba62ec952541
Gaston Roxas <gacaro51597@gmail.com>
2022-02-22 11:00:32 +0100

    added columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_165147) do
+ActiveRecord::Schema.define(version: 2022_02_22_095453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2022_02_21_165147) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "hogwarts_house"
+    t.integer "years_of_magic"
+    t.boolean "owner"
+    t.boolean "dark_magic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
